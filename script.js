8b3faa516b8d95a7ac1a286604b1062b307dc65a
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2022-07-25 03:23:59 -0300

    algumas mudancas no script e no css

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+const statusDisplay = document.querySelector('.gameStatus');
+
+let gameActive = true;
+
+let playerOne = "X";
+let playerTwo = "O";
+
 function mudarSimbolo() {
     var b1, b2, b3, b4, b5, b6, b7, b8, b9;
+    
     b1 = document.getElementById("b1").value;
     b2 = document.getElementById("b2").value;
     b3 = document.getElementById("b3").value;
@@ -9,9 +17,33 @@ function mudarSimbolo() {
     b7 = document.getElementById("b7").value;
     b8 = document.getElementById("b8").value;
     b9 = document.getElementById("b9").value;
+
+    /* 
+Arrays com as condicoes de vitória 
+
+  0 1 2
+  3 4 5
+  6 7 8
+
+    */
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];     
 }
 
-// Botão restart do jogo
+/* Validacao de vitoria */
+function validarVitoria() {
+    let vitoria = false;
+}
+
+/* Botão restart do jogo */
 function restart() {
     location.reload();
     b1 = document.getElementById("b1").value = ' ';
